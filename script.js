e64091b33add9d8222be89a497e7ec17dd79159d
marcospaulopaiva <marcos.poo@gmail.com>
2023-12-27 11:09:18 -0300

    Aula 4 - Desenvolvendo o temporizador.

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 musica.loop = true;
 
 musicaFocoInput.addEventListener('change', () => {
@@ -16,7 +26,6 @@ musicaFocoInput.addEventListener('change', () => {
         musica.pause()
     }
 })
-
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -41,24 +50,47 @@ function alterarContexto(contexto) {
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
         case 'foco':
-            titulo.innerHTML = `Otimize sua produtividade,<br>
-            <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `
+            titulo.innerHTML = `Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`
             break;
         
         case 'descanso-curto':
-            titulo.innerHTML = `Que tal dar uma respirada?<br>
-            <strong class="app__title-strong">Faça uma pausa curta!</strong>
-            `
+            titulo.innerHTML = `Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta!</strong>`
             break;
 
         case 'descanso-longo':
-            titulo.innerHTML = `Hora de voltar à superfície.<br>
-            <strong class="app__title-strong">Faça uma pausa longa.</strong>
-            `
+            titulo.innerHTML = `Hora de voltar à superfície.<br><strong class="app__title-strong">Faça uma pausa longa.</strong>`
             break;
     
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('Tempo: ' + tempoDecorridoEmSegundos)
+    console.log('Id: ' + intervaloId)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        audioPausa.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
